1ff93aca72d9cbcb4b8ef218653048c0d3222317
Atishay Jain <hritikjain707@gmail.com>
2021-03-24 11:41:22 +0530

    Feat: Hide Code Snippet

--- Components/script.js
+++ Components/script.js
@@ -1,10 +1,37 @@
 const sideBar = document.querySelector(".sidebarCont");
 const contentCont = document.querySelector(".contentCont");
 const body = document.querySelector("body");
+const codeToggler = document.querySelectorAll(".showCodeToggle");
+const code = document.querySelectorAll(".code");
 
 const toggleSidebar = () => {
   console.log("toggled");
   sideBar.classList.toggle("hideSidebar");
   contentCont.classList.toggle("overlay");
   body.classList.toggle("hideOverflow");
+};
+Array.from(codeToggler).forEach((el) => {
+  el.innerHTML = "Show Snippet ▼";
+});
+
+const showCodeToggle = (event) => {
+  // code.classList.toggle("hideCode");
+  // if (codeToggler.innerHTML == "Show Snippet ▼") {
+  //   codeToggler.innerHTML = "Hide Snippet ▲";
+  // } else {
+  //   codeToggler.innerHTML = "Show Snippet ▼";
+  // }
+  // console.log("looggg");
+  /**************** */
+  Array.from(code).forEach((el) => {
+    el.classList.toggle("hideCode");
+  });
+  Array.from(codeToggler).forEach((el) => {
+    if (el.innerHTML == "Show Snippet ▼") {
+      el.innerHTML = "Hide Snippet ▲";
+    } else {
+      el.innerHTML = "Show Snippet ▼";
+    }
+    console.log("looggg");
+  });
 };
